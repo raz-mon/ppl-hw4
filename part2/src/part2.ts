c39dcf9403561de18824b7a71295bc480904a3a2
raz-mon <razmon@post.bgu.ac.il>
2021-05-27 08:24:03 +0300

    part2 complete

--- part2/src/part2.ts
+++ part2/src/part2.ts
@@ -1,7 +1,5 @@
 /* 2.1 */
 
-import { isGeneratorFunction } from "util/types";
-
 export const MISSING_KEY = '___MISSING___'
 
 type PromisedStore<K, V> = {
@@ -49,7 +47,6 @@ export function makePromisedStore<K, V>(): PromisedStore<K, V> {
             retarr.push(val)).catch((err) => reject(MISSING_KEY)))
         resolve(retarr);
     })
-    // This contains a mutation on retarr! Is this o.k?
  }
 
 /* 2.2 */
@@ -93,22 +90,10 @@ export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: (t: T) => boo
  }
 
 /* 2.4 */
-// you can use 'any' in this question
 
 export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...((a: any) => Promise<any>)[]]): Promise<any> {
-    // We get a list of async functions, and apply them one after the other, where the first function gets no
-    // parameter, and the rest get as their parameter the return value of the prior function.
-    // If a function failes (i.e rejects, we are dealing with promises), then we retry. We retry at most twice.
-    // async function - a function that returns a Promise<T>. Can use await inside it's body.
-
-   // Not sure about the syntax of the declaration of the array components types! See if that actually works. 
-   // The meaning was that we want every other array component to be of the type: (any) => Promise<any>.
-
    const len: Number = fns.length;
-
    let last_val = await (fns[0])();
-   //console.log("first val: ", last_val);
-
    for (let i = 1; i < len; i++){
         let last_val1 = await tryFirst(last_val, fns[i]);
         if (last_val1 === -1000){
@@ -123,30 +108,6 @@ export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...((a: 
         else{
             last_val = last_val1;
         }
-
-
-
-/*
-    try{
-        last_val = await fns[i](last_val);
-        console.log("val after ", i, "computations: ", last_val);
-    }
-     catch{
-         console.log("first error occured");
-         setTimeout(() => {
-                 try{
-                     last_val = tryAgain(last_val, fns[i]);
-                 }
-                 catch{
-                     setTimeout(() => {
-                         last_val = tryThird(last_val, fns[i]);
-                     }, 2000);
-                 }
-             }, 2000);
-            // console.log(last_val);
-     }
-     */
-    // console.log(last_val);
  }
     async function tryFirst (last_val: any, func: any) {
         try{
@@ -154,9 +115,7 @@ export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...((a: 
             return x;
         }
         catch{
-            //console.log("First error occured");
             return -1000;
-            //throw Error();
         }
     }   
 
@@ -166,9 +125,7 @@ export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...((a: 
             return x;
         }
         catch{
-            //console.log("second error occured");
             return -1000;
-            //throw Error();
         }
     }   
 
@@ -178,107 +135,8 @@ export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...((a: 
             return x;
         }
         catch{
-            //console.log("third error occured");
-            throw Error();
+            throw Error();      // No more tolarence for errors (no more nice guy)!!
         }
-    }    
-
+    }
     return last_val;
 }
-
-
-
-// 
-///* 2.3 */
-//export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: ???): Generator<T> {
-//    // Don't quite understand the meaning of "don't convert the generator to an array".
-//    // Is this because it supposidly holds some numbers that I can treat as an array?
-//    // If so, all I need to do is compute the computation one-by-one. The returned generator takes a T from the given generator, 
-//    // performs the filter on it, and then returns it or not apropriatly. Isn't that exactly what we did in class??
-//    
-//    // If it is indeed identical to what we saw in class, then this should do the job:
-//    return function* newGen (): Generator<T> {
-//        for (let x of genFn()) {
-//            if (filterFn(x)) {
-//                yield x;
-//            }
-//        } 
-//    }
-//}
-//export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: ???): Generator<T> {
-//    // If the written above is correct, it is relevant to this section too.
-//    return function* newGen (): Generator<T> {
-//        for (let x of genFn()) {
-//            yield mapFn(x);
-//        }
-//    }
-//}
-//
-///* 2.4 */
-//// you can use 'any' in this question
-//
-// export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...(???)[]]): Promise<any> {
-//     ???
-// }
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export function makePromisedStore<K, V>(): PromisedStore<K, V> {
-//     ???
-//     return {
-//         get(key: K) {
-//             ???
-//         },
-//         set(key: K, value: V) {
-//             ???
-//         },
-//         delete(key: K) {
-//             ???
-//         },
-//     }
-// }
-
-// export function getAll<K, V>(store: PromisedStore<K, V>, keys: K[]): ??? {
-//     ???
-// }
-
-/* 2.2 */
-
-// ??? (you may want to add helper functions here)
-//
-// export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
-//     ???
-// }
-
-/* 2.3 */
-
-// export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: ???): ??? {
-//     ???
-// }
-
-// export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: ???): ??? {
-//     ???
-// }
-
-/* 2.4 */
-// you can use 'any' in this question
-
-// export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...(???)[]]): Promise<any> {
-//     ???
-// }
